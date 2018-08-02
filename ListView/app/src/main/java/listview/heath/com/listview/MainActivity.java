7d489d5fc4df1414217da547ed6182e32d18bf7f
xiayan <xiayan@autohome.com.cn>
2018-08-02 18:13:25 +0800

    add ListView

---
+++ ListView/app/src/main/java/listview/heath/com/listview/MainActivity.java
@@ -0,0 +1,61 @@
+package listview.heath.com.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+
+    // 创建数据源
+    private final static String[] strs = new String[] {"first", "second", "third", "forth", "fifth"
+    };
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+        // 绑定layout中的listView
+        listView = (ListView) findViewById(R.id.list_view);
+
+        ArrayList<HashMap<String, Object>> dataList = new ArrayList<HashMap<String, Object>>();
+        for (int i = 0; i < 10; i++) {
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            map.put("itemImg", R.drawable.ic_launcher_background);
+            map.put("itemTitle", "第几" + i + "行");
+            map.put("itemText", "This is " + i );
+            dataList.add(map);
+        }
+        Log.e("item Data ArrList", dataList.toString());
+        // 创建SimpleAdapter
+        SimpleAdapter adapter = new SimpleAdapter(this, dataList, R.layout.item, new String[]{"itemImg", "itemTitle", "itemText"}, new int[] {R.id.itemImg, R.id.itemTitle, R.id.itemText});
+        listView.setAdapter(adapter);
+
+        // 设置listview adapter 适配器
+        /***
+         * 第二个参数：自定义一个layout，系统有默认的样式，如果有choice或者check选择器 需要设置setMode否则不显示
+         */
+//        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, strs));
+        // 设置选择的Mode 否则不显示
+//        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        configListView();
+    }
+
+    // 设置listView监听者，并该接口方法
+    private void configListView () {
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        setTitle("你点击了" + i + "行");
+    }
+}
--- Preference/app/src/main/java/preference/heath/com/preference/MainActivity.java
+++ Preference/app/src/main/java/preference/heath/com/preference/MainActivity.java
@@ -1,25 +1,61 @@
 package preference.heath.com.preference;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = "HHHHHHHHH";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Context context = this.getApplicationContext();
+        Intent intent = getIntent();
+        context.startService(intent);
+        context.stopService(intent);
+        Log.e(TAG, "onCreate========");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        Log.e(TAG, "onStar========");
+    }
 
-        // Uri 功能跳转
-        Uri number = Uri.parse("tel:18501036904");
-        Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e(TAG, "onRestart======");
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "onResume========");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, "onPause======");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG, "onStop=======");
+    }
+
+    public void sendMessage (View view) {
+        Button button = (Button) view;
+        Log.e("MainActivity", button.getText().toString());
     }
 }
